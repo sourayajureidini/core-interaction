6b675e69e712b45dbb1474a8b87d44f6b21715ba
Souraya Jureidini <sourayajureidini@Sourayas-MBP.wireless.newschool.edu>
2018-03-27 11:01:54 -0400

    “anything”

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+// piece of code that does one or more actions
+
+/*
+ function go() {
+ alert ('hi')
+ alert('hey there')
+}
+
+
+// it didnt work first because we didnt name the function >
+
+go();
+go();
+
+
+ go(); is the name of the function , we can name a function as many times as we like
+
+
+function go(name, age) {
+  alert ('name')
+  alert('age')
+}
+// the term function is how you define a function
+//go is the name of the function
+// whats in the brackets is an argument
+// arguments can be used many times > called: DRY CODE
+
+go('Will',34);
+go('Souraya',22);
+
+// go (function name)+arguments
+
+var name = prompt();
+
+function add(first,second) {
+return first+second;
+}
+
+ var sum = add(1,2);
+ // sum is the name of the variable, add is the name of the function and (1,2) is whats in the function
+
+ */
---
+++ studio/assets/js/01_post.js
@@ -0,0 +1,8 @@
+var debtDiv = document.querySelector('div.debt')
+var myDebt = 'ideal'
+
+
+debtDiv.addEventListener('click' , function() {
+  myDebt = 'sexually passive'
+  debtDiv.innerText = 'sexually passive'
+})
