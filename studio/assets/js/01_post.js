c719795877c1400ffc7d1edb29a4bc8af2f65ce1
Souraya Jureidini <sourayajureidini@Sourayas-MBP.fios-router.home>
2018-03-30 18:59:11 -0400

    “anything”

--- studio/assets/js/01_post.js
+++ studio/assets/js/01_post.js
@@ -4,9 +4,17 @@ var myDebt = 'ideal'
 
 debtDiv.addEventListener('mouseover' , function() {
   myDebt = 'sexually passive'
-  debtDiv.innerText = 'sexually passive', s
+  debtDiv.innerText = 'sexually passive',
 })
 
 debtDiv.addEventListener('mouseout' , function() {
     debtDiv.innerText = '"the ideal women"'
+})
+
+window.addEventListener('click', function (event) {
+var dot = document.createElement('div')
+dot.classList.add('dot')
+dot.style.top = event.pageY + 'px'
+dot.style.left = event.pageX + 'px'
+document.body.appendChild(dot)
 })
